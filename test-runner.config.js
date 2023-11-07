3b11dae65a3494dc8d41a2d81858f5e2f5c73821
Radek Sedlák <r.sedlak@quadient.com>
2023-11-07 13:24:34 +0100

    move cli args to config

--- test-runner.config.js
+++ test-runner.config.js
@@ -4,4 +4,6 @@ import {jasmineTestRunnerConfig} from 'web-test-runner-jasmine';
 export default {
     ...jasmineTestRunnerConfig(),
     plugins: [esbuildPlugin({ts: true, target: 'auto'})],
+    nodeResolve: true,
+    files: "test/**/*.(test|spec).[t|j]s"
 };
